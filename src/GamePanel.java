d13ce99b748908e8007a12c93a11de98da8585af
BenRoselle <benjaminroselle15@gmail.com>
2024-07-25 15:09:04 +0100

    First Commit

---
+++ src/GamePanel.java
@@ -0,0 +1,11 @@
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class GamePanel  extends JPanel{
+
+    public GamePanel()
+    {
+        this.setPreferredSize(new Dimension(2000,2000));
+        this.setBackground(Color.green);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,29 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
 
-        }
+        JFrame f = createWindow();
+        GamePanel gp = new GamePanel();
+        f.add(gp);
+
+    }
+
+    public static JFrame createWindow()
+    {
+        //creates and returns JFrame
+        JFrame w = new JFrame("Window Name");
+        w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        w.setTitle("RTS Magic");
+        w.setSize(1600,1000);
+        w.setResizable(false);
+        w.setLayout(null);
+        w.setLocationRelativeTo(null);
+        w.setVisible(true);
+        return w;
     }
 }
